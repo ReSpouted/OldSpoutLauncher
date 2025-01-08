805d19d5529ff0510ca03f31c5785485ed9380ff
blackilykat <essenjey237@gmail.com>
2025-01-08 21:01:48 +0100

    Update OS to print info when detecting

--- src/main/java/org/respouted/util/OS.java
+++ src/main/java/org/respouted/util/OS.java
@@ -4,9 +4,15 @@ public enum OS {
 	UNIX,
 	WINDOWS;
 	
-	public static final OS CURRENT_OS = System.getProperty("os.name").startsWith("Windows") ? WINDOWS : UNIX;;
+	public static final OS CURRENT_OS;
 	
 	public static String getClasspathSeparator() {
 		return CURRENT_OS == WINDOWS ? ";" : ":";
+	}
+
+	static {
+		String name = System.getProperty("os.name");
+		CURRENT_OS = name.startsWith("Windows") ? WINDOWS : UNIX;
+		System.out.printf("Recognized OS '%s' as %s\n", name, CURRENT_OS);
 	}
 }
