eaa8e178df6edbb3089b45b7e060efad89553e24
blackilykat <essenjey237@gmail.com>
2025-01-08 23:43:24 +0100

    Make it print version at startup

--- src/main/java/org/respouted/Main.java
+++ src/main/java/org/respouted/Main.java
@@ -1,14 +1,17 @@
 package org.respouted;
 
 import org.respouted.auth.Authorizer;
+import org.respouted.util.Util;
 
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 
 public class Main {
-    public static LauncherMCP mcp = new LauncherMCP();
+    public static LauncherMCP mcp;
     public static LauncherWindow window = null;
     public static void main(String[] args) {
+        System.out.printf("OldSpoutLauncher v%s\n", Util.VERSION);
+        mcp = new LauncherMCP();
         SwingUtilities.invokeLater(() -> {
             window = new LauncherWindow(mcp);
             new SwingWorker<Void, Void>() {
